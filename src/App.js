824c7f99452877ffaaaab6de0230f95c138bef08
Fabio Bergmann <fabiocberg@gmail.com>
2021-11-12 10:44:03 -0300

    feat: initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+
+  const {register, handleSubmit, setValue, setFocus} = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e);
+  }
+
+  const checkCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {
+      console.log(data);
+      // register({ name: 'address', value: data.logradouro });
+      setValue('address', data.logradouro);
+      setValue('neighborhood', data.bairro);
+      setValue('city', data.localidade);
+      setValue('uf', data.uf);
+      setFocus('addressNumber');
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        CEP:
+        <input type="text" {...register("cep")} onBlur={checkCEP} />
+      </label>
+      <label>
+        Rua:
+        <input type="text" {...register("address" )}/>
+      </label>
+      <label>
+        Número:
+        <input type="text" {...register("addressNumber" )}/>
+      </label>
+      <label>
+        Bairro:
+        <input type="text" {...register("neighborhood" )}/>
+      </label>
+      <label>
+        Cidade:
+        <input type="text" {...register("city" )}/>
+      </label>
+      <label>
+        Estado:
+        <input type="text" {...register("uf" )}/>
+      </label>
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
